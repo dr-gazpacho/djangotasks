8a0391849bab74c4cd4449ba91ee6d5d60e2d5c1
dr-gazpacho <gonzaloandresgazpacho@gmail.com>
2025-02-27 19:45:37 -0600

    create two tables

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    user_name = models.CharField(max_length=200)
+    tasks_completed = models.IntegerField(default=0)
+
+class Tasks(models.Model):
+    task_name = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    date_added = models.DateTimeField()
+    date_completed = models.DateTimeField()
+    date_deadline = models.DateTimeField()
+    is_complete = models.BooleanField()
+    completed_by = models.ForeignKey(Users)
+
