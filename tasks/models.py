9dc549f746b969bb8ebf9eef8e17bf5826ce45e2
dr-gazpacho <gonzaloandresgazpacho@gmail.com>
2025-02-27 20:19:15 -0600

    update the models and run migrations

--- tasks/models.py
+++ tasks/models.py
@@ -1,15 +1,45 @@
 from django.db import models
+from django.utils import timezone
 
-class Users(models.Model):
-    user_name = models.CharField(max_length=200)
+class User(models.Model):
+    username = models.CharField(max_length=200, unique=True)
     tasks_completed = models.IntegerField(default=0)
 
-class Tasks(models.Model):
+    # this is for the debugger - without this it would return a generic name for this like User object (1)
+    def __str__(self):
+        return self.username
+    
+    # Django automatically creates a reverse relationship named modelname_set when you define a foreign key.
+    # Since we have a Task model with a foreign key to User, Django creates a task_set attribute on each User instance that provides access to all related Task objects. 
+    def completed_tasks_count(self):
+        return self.task_set.filter(is_complete=True).count()
+
+# adding lots of methods in here to let User access Task
+# from tasks: task = Task.objects.get(id=1)
+# user = task.completed_by
+class Task(models.Model):
     task_name = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
     date_added = models.DateTimeField()
     date_completed = models.DateTimeField()
     date_deadline = models.DateTimeField()
-    is_complete = models.BooleanField()
-    completed_by = models.ForeignKey(Users)
+    is_complete = models.BooleanField(default=False)
+
+    completed_by = models.ForeignKey(
+        User, 
+        on_delete=models.SET_NULL,  # If a user is deleted, we don't delete the task
+        related_name='completed_tasks',
+        null=True,
+        blank=True
+    )
+
+    def is_task_complete(self, user):
+        """Mark task as complete by specified user"""
+        self.is_complete = True
+        self.completed_by = user
+        self.date_completed = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
 
